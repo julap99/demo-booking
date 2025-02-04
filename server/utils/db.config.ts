ae7b8f113b6fb589fb5fc82d9c062a4e0ddf735c
Haqeem Solehan <93633646+julap99@users.noreply.github.com>
2025-02-05 01:54:45 +0800

    jlp

--- server/utils/db.config.ts
+++ server/utils/db.config.ts
@@ -1,7 +1,7 @@
 export const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'your_database_name',
+  host: process.env.DB_HOST || '218.208.91.135',
+  user: process.env.DB_USER || 'stitchshutter_app',
+  password: process.env.DB_PASSWORD || '84Dtlm1={1;M',
+  database: process.env.DB_NAME || 'stitchshutter_app',
   port: Number(process.env.DB_PORT) || 3306
 } 
